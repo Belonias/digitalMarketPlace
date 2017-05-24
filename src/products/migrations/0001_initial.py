fd2c9c7f651e063e06f993b37255c5542ce8a7aa
Belonias <iakovosbelonias@gmail.com>
2017-05-25 00:39:00 +0300

    simple models

---
+++ src/products/migrations/0001_initial.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Product',
+            fields=[
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
+                ('title', models.CharField(max_length=120)),
+            ],
+        ),
+    ]
