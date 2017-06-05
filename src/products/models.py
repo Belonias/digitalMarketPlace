327533185e08282a4a20bf634bd37b9e2a4cf916
Belonias <iakovosbelonias@gmail.com>
2017-06-05 22:03:08 +0300

    slugs added on signal, signal pre_save

--- src/products/models.py
+++ src/products/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.db.models.signals import pre_save, post_save
+from django.utils.text import slugify
 
 # Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length=120)
-    slug = models.SlugField(default='slug-field')  # unique=True
+    slug = models.SlugField(blank=True)  # unique=True
     description = models.TextField(null=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
     sale_price = models.DecimalField(max_digits=6, decimal_places=2,
@@ -11,3 +13,18 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+
+def product_pre_save_receiver(sender, instance, *args, **kwargs):
+    print(sender)
+    print(instance)
+    if not instance.slug:
+        instance.slug = slugify(instance.title)
+
+pre_save.connect(product_pre_save_receiver, sender=Product)
+
+
+# def product_post_save_receiver(sender, instance, *args, **kwargs):
+#     if instance.slug != slugify(instance.title):
+#         instance.slug = slugify(instance.title)
+#         instance.save()
