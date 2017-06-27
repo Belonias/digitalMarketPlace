4bd270a503c527762ac1daf4802344a094907dcc
Belonias <iakovosbelonias@gmail.com>
2017-06-28 00:36:05 +0300

    move MultiSlugMixin in digitalmarketplace(projectFolder)/mixin.py

--- src/products/views.py
+++ src/products/views.py
@@ -8,23 +8,9 @@ from django.views.generic import (
     CreateView,
     UpdateView,
     DeleteView,
-)
+    )
+from digitalmarket.mixins import MultiSlugMixin
 # Create your views here.
-class MultiSlugMixin(object):
-    model = None
-
-    def get_object(self, *args, **kwargs):
-        print(self.kwargs)  # related to request
-        ModelClass = self.model
-        slug = self.kwargs.get('slug')
-        if slug is not None:
-            try:
-                obj = get_object_or_404(ModelClass, slug=slug)
-            except ModelClass.MultipleObjectsReturned:
-                obj = ModelClass.objects.filter(slug=slug).order_by('-sale_price').first()
-        else:
-            obj = super(MultiSlugMixin, self).get_object(*args, **kwargs)
-        return obj
 
 class ProductListView(ListView):
     model = Product
